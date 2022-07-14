3027c233cf2da7388a1f34234563f64ce1688c65
Christopher Gholmieh <toplogotreespam@gmail.com>
2022-07-14 09:21:28 -0700

    bot.py Change

--- bot/e.py
+++ bot/e.py
@@ -7,9 +7,6 @@ import sc2
 
 # Modules:
 
-from sc2.ids.unit_typeid import UnitTypeId
-from sc2.units import Units
-from sc2.unit import Unit
 from sc2.bot_ai import BotAI
 from sc2.data import Race
 
@@ -30,7 +27,6 @@ class CompetitiveBot(BotAI):
     
     # Initialization:
     def __init__(self):
-        self.Override: bool = False
         self.Tactics: dict = {
             Race.Protoss: ['3-1-1'],
             Race.Terran: ['1-1-1'],
@@ -46,20 +42,6 @@ class CompetitiveBot(BotAI):
             return True
         else:
             return False
-    
-    # Tactic Methods:
-    async def SCVs(self, Limit: int) -> bool:
-        CommandCenters: Units = self.townhalls.filter(lambda CC: CC.is_idle and CC.is_ready)
-        if CommandCenters and self.can_afford(UnitTypeId.SCV):
-            CommandCenter: Unit = CommandCenters.random
-            if self.units.of_type(UnitTypeId.SCV).amount < Limit:
-                CommandCenter.train(UnitTypeId.SCV)
-                return True
-            return True
-        return False
-    
-    async def Depots(self, Limit: int):
-        pass
 
     # Methods:
     async def on_start(self):
@@ -78,15 +60,8 @@ class CompetitiveBot(BotAI):
 
             # Protocols:
             if self.Compare(self.Tactic, '3-1-1'):
-                if self.Compare(self.Override, False):
-                    print(f"Iteration: {iteration}")
+                pass
             elif self.Compare(self.Tactic, '2-1-1'):
-                if self.Compare(self.Override, False):
-                    Sequence = Foundation([
-                        self.SCVs(13),
-                        
-                    ])
-                    await Sequence.Execute()
+                print(f"Iteration is: {iteration}")
             elif self.Compare(self.Tactic, '1-1-1'):
-                if self.Compare(self.Override, False):
-                    print(f"Iteration: {iteration}")
+                pass
