b59ce47a1b13d1645171b756506d311ad0e29b2b
Christopher Gholmieh <toplogotreespam@gmail.com>
2022-07-15 09:27:31 -0700

    Working prototype, finish protocol methods

--- bot/bot.py
+++ bot/bot.py
@@ -49,17 +49,31 @@ class CompetitiveBot(BotAI):
     
     # Tactic Methods:
     async def SCVs(self, Limit: int) -> bool:
-        CommandCenters: Units = self.townhalls.filter(lambda CC: CC.is_idle and CC.is_ready)
-        if CommandCenters and self.can_afford(UnitTypeId.SCV):
-            CommandCenter: Unit = CommandCenters.random
-            if self.units.of_type(UnitTypeId.SCV).amount < Limit:
+        if self.units.of_type(UnitTypeId.SCV).amount < Limit:
+            CommandCenters: Units = self.townhalls.filter(lambda CC: CC.is_idle and CC.is_ready)
+            if CommandCenters and self.can_afford(UnitTypeId.SCV):
+                CommandCenter: Unit = CommandCenters.random
                 CommandCenter.train(UnitTypeId.SCV)
                 return True
+            elif not self.townhalls.filter(lambda CommandCenter: CommandCenter.is_ready):
+                return False
             return True
-        return False
-    
-    async def Depots(self, Limit: int):
-        pass
+        return True
+    async def Depots(self, Limit: int) -> bool:
+        Total: int = self.structures.of_type({UnitTypeId.SUPPLYDEPOT, UnitTypeId.SUPPLYDEPOTLOWERED}).amount
+        if Total < Limit and self.can_afford(UnitTypeId.SUPPLYDEPOT):
+            if Limit <= 2:
+                Placement = self.DepotPlacements.pop()
+                SCV: Unit = self.workers.filter(lambda Worker: not Worker.is_returning).closest_to(Placement)
+                if SCV:
+                    SCV.build(UnitTypeId.SUPPLYDEPOT, Placement)
+                    return True
+                else:
+                    return False
+                #Worker: Unit = self.workers.filter(lambda x: not x.is_returning).closest_to(Position)
+            else:
+                return True
+        return True
 
     # Methods:
     async def on_start(self):
@@ -76,6 +90,16 @@ class CompetitiveBot(BotAI):
     async def on_step(self, iteration: int):
         if self.townhalls:
 
+            # Variable Management:
+
+            # Depots:
+            self.DepotPlacements = self.main_base_ramp.corner_depots
+            self.ExistingDepots: Units = self.structures.of_type({UnitTypeId.SUPPLYDEPOT, UnitTypeId.SUPPLYDEPOTLOWERED})
+
+            if self.ExistingDepots:
+                self.ExistingDepots = {SupplyDepot for SupplyDepot in self.DepotPlacements
+                    if self.ExistingDepots.closest_distance_to(SupplyDepot) > 1}
+            
             # Protocols:
             if self.Compare(self.Tactic, '3-1-1'):
                 if self.Compare(self.Override, False):
@@ -84,7 +108,8 @@ class CompetitiveBot(BotAI):
                 if self.Compare(self.Override, False):
                     Sequence = Foundation([
                         self.SCVs(13),
-                        
+                        self.Depots(1),
+                        self.SCVs(14)
                     ])
                     await Sequence.Execute()
             elif self.Compare(self.Tactic, '1-1-1'):
